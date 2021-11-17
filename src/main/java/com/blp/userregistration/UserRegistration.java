aab8dc34bea5ebeb3877e4a66b191bdfb16f90b0
Asus <dhanashreehakke7@gmail.com>
2021-11-17 15:12:58 +0530

    Added: Welcome message is added

---
+++ src/main/java/com/blp/userregistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.blp.userregistration;
+
+/**
+ * User registration program using lambda expression.
+ */
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration program");
+    }
+}
