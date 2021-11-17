22e68dd9785772c06a54a36989906bb4855ee9b3
Asus <dhanashreehakke7@gmail.com>
2021-11-17 15:41:10 +0530

    Added: Valid Firstname file is added

--- src/main/java/com/blp/userregistration/UserRegistration.java
+++ src/main/java/com/blp/userregistration/UserRegistration.java
@@ -1,11 +1,27 @@
 package com.blp.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  * User registration program using lambda expression.
  */
+@FunctionalInterface
+interface firstname {
+    void validUser(String name);
+}
+
 public class UserRegistration {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
+        firstname fn = (name)->{
+            boolean check = Pattern.matches("([A-Z][a-z]{3,})",name);
+            if (check == true) {
+                System.out.println("Firstname is Valid");
+            }else
+                System.out.println("Enter valid name");
+        };
+        fn.validUser("Dhanashree");
     }
 }
