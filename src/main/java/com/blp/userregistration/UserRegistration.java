87f37236e0b577c8fa07e50e53fb49fd6111ac65
Asus <dhanashreehakke7@gmail.com>
2021-11-17 16:55:35 +0530

    Added: Valid Password rule4 file is added

--- src/main/java/com/blp/userregistration/UserRegistration.java
+++ src/main/java/com/blp/userregistration/UserRegistration.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
  * User registration program using lambda expression.
  */
 @FunctionalInterface
-interface PasswordRule3 {
+interface PasswordRule4 {
     void validUser(String password);
 }
 
@@ -15,8 +15,8 @@ public class UserRegistration {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
-        PasswordRule3 fn = (password) -> {
-            boolean check = Pattern.matches("^([A-Z]{1,}[a-z0-9@#$|]{6}[0-9]{1,})$", password);
+        PasswordRule4 fn = (password) -> {
+            boolean check = Pattern.matches("^([A-Z]{1,}[a-z]{4}[@#$|]{1}[0-9]{1,})$", password);
             if (check == true) {
                 System.out.println("Password is Valid");
             } else
